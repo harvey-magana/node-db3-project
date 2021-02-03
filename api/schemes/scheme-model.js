5d90367d539af6f72c2f83c219a98c04648f4d85
harvey magana <heyharv2012@gmail.com>
2021-02-02 21:18:25 -0800

    Updated the queries.sql file with the correct queries for each question on what to run and created the scheme-model.js file.

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,42 @@
 // scheme-model
+const db = require('../../data/db-config.js');
+
+module.exports = {
+    /**
+     * find, 
+     * findById, 
+     * findSteps, 
+     * add, 
+     * update, 
+     * remove
+     */
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+    return db('steps as s')
+        .join('schemes as sc', 'sc.id', 's.step_id')
+        .where({ step_id: id })
+        .select('s.id', 'sc.scheme_name', 's.step_number', 's.instructions');
+}
+
+async function add(schemesData) {
+    const ids = await db('schemes').insert(schemesData);
+    return newScheme = await findById(ids[0]);
+}
+
+async function update(id, changes) {
+    await db('schemes').where({ id }).update(changes);
+    return await findById(id);
+}
+
+async function remove(id) {
+    return await db('schemes').del().where({ id });
+}
